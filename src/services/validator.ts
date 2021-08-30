df22a8dac1c7ae14eb54949c94d30e0dc4b8abed
HoukasaurusRex <jt@houk.space>
2021-08-30 15:52:24 +0800

    chore(ref): added friendlier message to validation error handler

--- src/services/validator.ts
+++ src/services/validator.ts
@@ -3,20 +3,17 @@ import { validationResult } from 'express-validator'
 
 export class ValidationError extends Error {
   public status = 400
-
   constructor(message: string, status?: number) {
     super(message)
     this.name = 'ValidationError'
-    if (status) {
-      this.status = status
-    }
+    this.status = status || this.status
   }
 }
 
 export default (req: Request, _res: Response, next: NextFunction): void => {
   const errors = validationResult(req)
   if (!errors.isEmpty()) {
-    const message = errors.array()[0].msg
+    const message = `Error validating request: ${errors.array()[0].msg}`
     const error = new ValidationError(message)
     next(error)
   }
